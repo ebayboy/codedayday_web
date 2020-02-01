c2d560b973a4a4d6f6eb25c23a181f3b83e71d03
fanpf <ebayboy@aliyun.com>
2020-02-01 21:07:32 +0800

    update for dva

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 codedayday_web
 
 - 1. taro init myapp
-
 - 2. npm install taro-ui
-
 - 3. npm i dva-core dva-loading --save
+- 4. npm install nervjs
+- 5. npm install --save react-redux
+- 6. npm i redux --sav
--- codedayday_web/src/app.tsx
+++ codedayday_web/src/app.tsx
@@ -5,6 +5,15 @@ import "./app.scss";
 
 import "taro-ui/dist/style/index.scss";
 
+import models from "./models";
+import dva from "./utils/dva";
+const dvaApp = dva.createApp({
+  initialState: {},
+  models: models
+});
+const store = dvaApp.getStore();
+
+
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
